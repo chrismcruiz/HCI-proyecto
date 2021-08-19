334de3e22d6715a8811278a2dab6a84cbe92195b
Christian Ruiz Lagos <chrismcruiz@gmail.com>
2021-08-19 21:44:46 +0200

    tabs agregadas en home

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,183 +1,211 @@
 import React, { useState } from "react";
-import {
-    Input,
-    SubmitButton,
-} from "./accountBox/common";
-import {filtrarUser, recorrerObjeto} from '../utils/Utils'
-import axios from 'axios'
-import { CircularProgress } from '@material-ui/core';
+import { Input, SubmitButton } from "./accountBox/common";
+import { filtrarUser, recorrerObjeto } from "../utils/Utils";
+import axios from "axios";
+import { CircularProgress } from "@material-ui/core";
 
 const EditProfile = (props) => {
-    props = props.props;
-   
-    const user = recorrerObjeto(filtrarUser(props.users, props.idUser))
-    const img = recorrerObjeto(filtrarUser(props.users, props.idUser)).photo
+  props = props.props;
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [editDescription, setEditDescription] = useState(user.description);
-    const [editPhoto, setEditPhoto] = useState(img.toString());
-    const [editName, setEditName] = useState(user.name);
-    const [editEmail, setEditEmail] = useState(user.email);
-    const [editBirthday, setEditBirthday] = useState(user.birthday);
-    const [editCareer, setEditCareer] = useState(user.career);
+  const user = recorrerObjeto(filtrarUser(props.users, props.idUser));
+  const img = recorrerObjeto(filtrarUser(props.users, props.idUser)).photo;
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [editDescription, setEditDescription] = useState(user.description);
+  const [editPhoto, setEditPhoto] = useState(img.toString());
+  const [editName, setEditName] = useState(user.name);
+  const [editEmail, setEditEmail] = useState(user.email);
+  const [editBirthday, setEditBirthday] = useState(user.birthday);
+  const [editCareer, setEditCareer] = useState(user.career);
 
-    function onTextboxChangeUpdateNombre(e) {
-        setEditName(e.target.value)
-    }
-    function onTextboxChangeUpdateEmail(e) {
-        setEditEmail(e.target.value)
-    }
-    function onTextboxChangeUpdateFecha(e) {
-        setEditBirthday(e.target.value)
-    }
-    function onTextboxChangeUpdateDescription(e) {
-        setEditDescription(e.target.value)
-    }
-    function onTextboxChangeUpdateCarrera(e) {
-        setEditCareer(e.target.value)
-    }
+  function onTextboxChangeUpdateNombre(e) {
+    setEditName(e.target.value);
+  }
+  function onTextboxChangeUpdateEmail(e) {
+    setEditEmail(e.target.value);
+  }
+  function onTextboxChangeUpdateFecha(e) {
+    setEditBirthday(e.target.value);
+  }
+  function onTextboxChangeUpdateDescription(e) {
+    setEditDescription(e.target.value);
+  }
+  function onTextboxChangeUpdateCarrera(e) {
+    setEditCareer(e.target.value);
+  }
 
-    const onPhotoChangeUpdatePhoto = (e) => {
-        setEditPhoto(e.target.files[0])
-    }
+  const onPhotoChangeUpdatePhoto = (e) => {
+    setEditPhoto(e.target.files[0]);
+  };
 
-    const onEdit = async (e) => {
-        // e.preventDefault();
-        setIsLoading(true)
-        
-        const formData = new FormData();
+  const onEdit = async (e) => {
+    // e.preventDefault();
+    setIsLoading(true);
 
-        formData.append('_id', user._id)
-        formData.append('photo', editPhoto)
-        formData.append('name', editName)
-        formData.append('email', editEmail)
-        formData.append('birthday', editBirthday)
-        formData.append('description', editDescription)
-        formData.append('career', editCareer)
-       
+    const formData = new FormData();
 
-        await axios.put('http://localhost:4000/app/update/', formData)
-            .then(response => {
-                console.log(response.data);
-                setIsLoading(false)
-                window.location.reload()
-                
-            })
-            .catch(error => {
-                console.log(error);
-                setIsLoading(false)
-            });
-    }
+    formData.append("_id", user._id);
+    formData.append("photo", editPhoto);
+    formData.append("name", editName);
+    formData.append("email", editEmail);
+    formData.append("birthday", editBirthday);
+    formData.append("description", editDescription);
+    formData.append("career", editCareer);
 
-    if (isLoading) {
-        return (<div className="vertical-center"><CircularProgress color="primary" size={60} /></div>)
-    }
+    await axios
+      .put("http://localhost:4000/app/update/", formData)
+      .then((response) => {
+        console.log(response.data);
+        setIsLoading(false);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
 
+  if (isLoading) {
     return (
+      <div className="vertical-center">
+        <CircularProgress color="primary" size={60} />
+      </div>
+    );
+  }
 
-        <div className='p-3'>
-            <div className='fondo-blanco pantalla_match px-4'>
-                <div className='div_imagen_edit_perfil'>
-                    <img className='imagen_persona_perfil' alt="" src={`/images/${img}`} />
-                </div>
-            </div>
-            <form method='POST' className='' encType='multipart/form-data'>
-
-                <div className='input-group mb-3'>
-                    <Input
-                        className='label_inputs form-control'
-                        type="file"
-                        name='photo'
-                        accept=".png, .jpg, .jpeg"
-                        id='photo'
-                        required
-                        onChange={onPhotoChangeUpdatePhoto}
-                    />
-                    <label htmlFor="photo" className='label_inputs input-group-text'>Cambiar imagen</label>
-                </div>
-
-
-                <div className="form-floating mb-3">
-                    <textarea
-                        id='description'
-                        className='form-control'
-                        placeholder='Añade una descripción breve de tí...'
-                        value={editDescription}
-                        onChange={onTextboxChangeUpdateDescription}
-                        style={{height: "100px"}}
-                    ></textarea>
-                    <label className="label_inputs" htmlFor='description'>Añade una breve descripción tuya...</label>
-                </div>
-
-                
-
-                <div className="input-group mb-2">
-                    <span class="input-group-text label_inputs">Nombre</span>
-                    <Input
-                    className='form-control'
-                    type="text"
-                    name="name"
-                    placeholder="Nombre"
-                    value={editName}
-                    onChange={onTextboxChangeUpdateNombre}
-                    required
-                    />
-                </div>
-                
-                 <div className="input-group mb-3">
-                    <span class="label_inputs input-group-text">Correo</span>
-                    <Input
-                    className='form-control'
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    value={editEmail}
-                    onChange={onTextboxChangeUpdateEmail}
-                    required
-                    />
-                </div>
-                
-                <div className='form-floating mb-3'>
-                    <Input
-                        className='form-control label_inputs'
-                        type="date"
-                        name="birthday"
-                        placeholder="Fecha de nacimiento"
-                        value={editBirthday}
-                        onChange={onTextboxChangeUpdateFecha}
-                        required
-                    />
-                    <label className='label_inputs'>Fecha de nacimiento</label>
-                </div>
-
-                <div className='form-floating'>
-                    <select
-                        required
-                        className="form-select"
-                        name='career'
-                        id='career'
-                        value={editCareer}
-                        onChange={onTextboxChangeUpdateCarrera}
-                    >
-                        <option value='' selected disabled>Escoge una opción</option>
-                        <option value='ingenieria de sistemas'>Ingeniería de Sistemas</option>
-                        <option value='ingenieria industrial'>Ingeniería Industrial</option>
-                        <option value='ingenieria de petroleos'>Ingeniería de Petroleos</option>
-                        <option value='ingenieria civil'>Ingeniería Civil</option>
-                        <option value='ingenieria metalurgica'>Ingeniería Metalúrgica</option>
-                        <option value='ingenieria electronica'>Ingeniería Electrónica</option>
-                        <option value='licenciatura en idiomas'>Licenciatura en Idiomas</option>
-                    </select>
-                    <label htmlFor="career" className='label_inputs'>Carrera</label>
-                </div>
-                <SubmitButton className='mt-3' type="submit" value='submit' onClick={onEdit} >Guardar</SubmitButton>
-            </form>
-            <div className="d-flex justify-content-center fondo-blanco pt-4">
-                <p className='text-danger font-weight-bold h4 m-0 py-3 boton_salir' onClick={props.logOut} >Cerrar sesion</p>
-            </div>
+  return (
+    <div className="p-3">
+      <div className="fondo-blanco pantalla_match px-4 mb-3">
+        <div className="div_imagen_edit_perfil">
+          <img
+            className="imagen_persona_perfil"
+            alt=""
+            src={`/images/${img}`}
+          />
         </div>
-    )
-}
+      </div>
+      <form method="POST" className="" encType="multipart/form-data">
+        <div className="input-group mb-3">
+          <Input
+            className="label_inputs form-control"
+            type="file"
+            name="photo"
+            accept=".png, .jpg, .jpeg"
+            id="photo"
+            required
+            onChange={onPhotoChangeUpdatePhoto}
+          />
+          <label htmlFor="photo" className="label_inputs input-group-text">
+            Cambiar imagen
+          </label>
+        </div>
 
-export default EditProfile
+        <div className="form-floating mb-3">
+          <textarea
+            id="description"
+            className="form-control"
+            placeholder="Añade una descripción breve de tí..."
+            value={editDescription}
+            onChange={onTextboxChangeUpdateDescription}
+            style={{ height: "100px" }}
+          ></textarea>
+          <label className="label_inputs" htmlFor="description">
+            Añade una breve descripción tuya...
+          </label>
+        </div>
+
+        <div className="input-group mb-2">
+          <span class="input-group-text label_inputs">Nombre</span>
+          <Input
+            className="form-control"
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            value={editName}
+            onChange={onTextboxChangeUpdateNombre}
+            required
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span class="label_inputs input-group-text">Correo</span>
+          <Input
+            className="form-control"
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={editEmail}
+            onChange={onTextboxChangeUpdateEmail}
+            required
+          />
+        </div>
+
+        <div className="form-floating mb-3">
+          <Input
+            className="form-control label_inputs"
+            type="date"
+            name="birthday"
+            placeholder="Fecha de nacimiento"
+            value={editBirthday}
+            onChange={onTextboxChangeUpdateFecha}
+            required
+          />
+          <label className="label_inputs">Fecha de nacimiento</label>
+        </div>
+
+        <div className="form-floating">
+          <select
+            required
+            className="form-select"
+            name="career"
+            id="career"
+            value={editCareer}
+            onChange={onTextboxChangeUpdateCarrera}
+          >
+            <option value="" selected disabled>
+              Escoge una opción
+            </option>
+            <option value="ingenieria de sistemas">
+              Ingeniería de Sistemas
+            </option>
+            <option value="ingenieria industrial">Ingeniería Industrial</option>
+            <option value="ingenieria de petroleos">
+              Ingeniería de Petroleos
+            </option>
+            <option value="ingenieria civil">Ingeniería Civil</option>
+            <option value="ingenieria metalurgica">
+              Ingeniería Metalúrgica
+            </option>
+            <option value="ingenieria electronica">
+              Ingeniería Electrónica
+            </option>
+            <option value="licenciatura en idiomas">
+              Licenciatura en Idiomas
+            </option>
+          </select>
+          <label htmlFor="career" className="label_inputs">
+            Carrera
+          </label>
+        </div>
+        <SubmitButton
+          className="mt-3"
+          type="submit"
+          value="submit"
+          onClick={onEdit}
+        >
+          Guardar
+        </SubmitButton>
+      </form>
+      <div className="d-flex justify-content-center fondo-blanco pt-4">
+        <p
+          className="text-danger font-weight-bold h4 m-0 py-3 pointer"
+          onClick={props.logOut}
+        >
+          Cerrar sesion
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default EditProfile;
--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -1,58 +1,138 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios'
+import axios from "axios";
 
 const Matches = (props) => {
+  props = props.props;
 
-    props = props.props
+  const [aMatchesUser, setMatchesUser] = useState([]);
+  const [mostrarMatches, setMostrarMatches] = useState(true);
+  const [mostrarGrupos, setMostrarGrupos] = useState(false);
 
-    const [aMatchesUser, setMatchesUser] = useState([]);
-    const [mostrarMatches, setMostrarMatches] = useState(false);
+  useEffect(() => {
+    const matches = async () => {
+      const req = await axios.post("http://localhost:4000/app/users/match", {
+        _id: props.idUser,
+      });
 
-    useEffect(() => {
-        const matches = async () => {
-            const req = await axios.post("http://localhost:4000/app/users/match",{
-                _id: props.idUser
-            });
-            
-            if (req.data) {
-                let aMatches = req.data;
-                const req2 = await axios.post("http://localhost:4000/app/matches",{
-                    matches: aMatches
-                });
-                if(req2.data){
-                    setMatchesUser(req2.data);
-                }
-            }
+      if (req.data) {
+        let aMatches = req.data;
+        const req2 = await axios.post("http://localhost:4000/app/matches", {
+          matches: aMatches,
+        });
+        if (req2.data) {
+          setMatchesUser(req2.data);
         }
-        matches()
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+      }
+    };
+    matches();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const mostrarMisMatch = () => {
-        setMostrarMatches(!mostrarMatches)
-    }
+  const muestraGrupos = () => {
+    setMostrarMatches(false);
+    setMostrarGrupos(true);
+  };
 
-    return (
-        <div className='fondo-blanco pantalla_match p-3'>
-            <div className="d-flex justify-content-between">
+  const muestraMatches = () => {
+    setMostrarMatches(true);
+    setMostrarGrupos(false);
+  };
+
+  return (
+    <div className="fondo-blanco pantalla_match p-3">
+      {/* <div className="d-flex justify-content-between">
                 <div onClick={mostrarMisMatch} className="p-2 bd-highlight pointer">Matches</div>
                 <div className="p-2 bd-highlight pointer">Mis grupos</div>
-            </div> 
-            {mostrarMatches ? 
-                <div className='div_personas_matches'>
-                    {aMatchesUser.map((match, index) =>
-                        <div key={index} className='div_imagen_personas_matches position-relative m-2 d-inline-block'>
-                            <img className='imagen_personas_matches' alt="" src={`/images/${match.photo}`} />
-                            <label className='position-absolute label_nombre_matches texto-blanco fw-bold'>{match.name}</label>
-                        </div>
-                    )}
-                </div>
-            :
-                ''
-            }
+            </div>  */}
+
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home"
+            type="button"
+            role="tab"
+            aria-controls="home"
+            aria-selected="true"
+          >
+            Matches
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile"
+            type="button"
+            role="tab"
+            aria-controls="profile"
+            aria-selected="false"
+          >
+            Grupos
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="contact-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#contact"
+            type="button"
+            role="tab"
+            aria-controls="contact"
+            aria-selected="false"
+          >
+            Mis Grupos
+          </button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div
+          class="tab-pane fade show active"
+          id="home"
+          role="tabpanel"
+          aria-labelledby="home-tab"
+        >
+          <div className="div_personas_matches">
+            {aMatchesUser.map((match, index) => (
+              <div
+                key={index}
+                className="div_imagen_personas_matches position-relative m-2 d-inline-block"
+              >
+                <img
+                  className="imagen_personas_matches"
+                  alt=""
+                  src={`/images/${match.photo}`}
+                />
+                <label className="position-absolute label_nombre_matches texto-blanco fw-bold">
+                  {match.name}
+                </label>
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
+        <div
+          class="tab-pane fade"
+          id="profile"
+          role="tabpanel"
+          aria-labelledby="profile-tab"
+        >
+          <h6 className="mt-3">No hay grupos para mostrar</h6>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="contact"
+          role="tabpanel"
+          aria-labelledby="contact-tab"
+        >
+          <h6 className="mt-3">No hay grupos para mostrar</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Matches
+export default Matches;
