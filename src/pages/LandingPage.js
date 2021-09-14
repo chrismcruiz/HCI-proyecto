151f1e9c8e898b0efcba1ea49a912ddbb05405ee
Christian Ruiz Lagos <chrismcruiz@gmail.com>
2021-09-13 22:24:18 -0500

    minimal changes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,10 @@ import React from 'react'
 const LandingPage = () => {
     return (
         <div>
+            <div style={{display: "flex", justifyContent:"flex-end"}}>
+                    <a className="me-3 mt-2" href="/login">Entrar</a>
+                    <a className="me-3 mt-2" href="/signup">Regístrarme</a>
+            </div>
             <h1>Aquí va una landing page :D</h1>
         </div>
     )
