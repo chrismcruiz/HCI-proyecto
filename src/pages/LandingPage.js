247d0de517ed0d85947019d2a0890b4f030180bb
Jean C <jeankporti17@gmail.com>
2021-09-20 23:10:49 -0500

    viva el lolcito agregado

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import './LandingPage.css'
 
+// Todo va dentro del "div"
+// Entonces básicamente ud mete el html que quiera ahí dentro y le da clases para darle estilos sólo que en ves del atributo "class"
+// usa el atributo "className"
 const LandingPage = () => {
     return (
         <div>
@@ -8,6 +12,7 @@ const LandingPage = () => {
                     <a className="me-3 mt-2" href="/signup">Regístrarme</a>
             </div>
             <h1>Aquí va una landing page :D</h1>
+            <h2 className="lolcito">Viva el lolcito</h2>
         </div>
     )
 }
